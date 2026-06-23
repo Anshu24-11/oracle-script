da7c3b63bdb2981de1fdf817aad3e70db13df6af
Jaydipchangani <jaydip.jsdev@gmail.com>
2026-06-23 12:09:36 +0530

    fix: start.js change

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
-fs.writeFileSync("./private.pem", process.env.OCI_PRIVATE_KEY);
+const privateKey = process.env.OCI_PRIVATE_KEY;
+
+fs.writeFileSync("./private.pem", privateKey);
 
 process.env.PRIVATE_KEY_PATH = "./private.pem";
 
